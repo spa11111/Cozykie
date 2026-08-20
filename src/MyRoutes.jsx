29bfcfd7390b693094793e0abdc839fc70f5fb46
spa11111 <sparsha612@gmail.com>
2026-08-20 09:45:31 +0545

    another fix

--- src/MyRoutes.jsx
+++ src/MyRoutes.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/user/Home";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
 import PageNotFound from "./pages/PageNotFound";
-import About from "./pages/user/About";
 import RecipeScaler from "./pages/user/tools/RecipeScaler";
 import MeasurementConverter from "./pages/user/tools/MeasurementConverter";
 import PantrySubstitution from "./pages/user/tools/PantrySubstitution";
@@ -31,7 +30,6 @@ const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
       <Route path="/create" element={<Signup />} />
 
